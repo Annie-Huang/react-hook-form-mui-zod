0b20d407738d6ebcce85e14642f096a2f4394532
Annie Huang <annie.hn@gmail.com>
2024-08-18 16:38:55 +1000

    Explain formState, useForm and mode

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,7 +1,21 @@
 import { useForm } from 'react-hook-form';
 
 export const Users = () => {
-  const { register } = useForm<{ email: string }>();
+  const {
+    register,
+    formState: { errors },
+  } = useForm<{ email: string }>({ mode: 'all' }); // also have a onBlur mode
 
-  return <input {...register('email')} placeholder='Email' />;
+  return (
+    <>
+      <input
+        {...register('email', {
+          required: { value: true, message: 'The email is required' },
+          maxLength: { value: 10, message: 'Too many characters' },
+        })}
+        placeholder='Email'
+      />
+      <p>{errors.email?.message}</p>
+    </>
+  );
 };
